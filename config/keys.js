21e5747bdbdb1b426aa21c2ada476b713a595cb2
jennifermorgan2016 <jennifer.morgan@ise.ie>
2018-10-11 17:03:30 +0100

    passport-jwt 1:45

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://dbuser:dbuser@127.0.0.1:27017/devconnector"
+  mongoURI: "mongodb://dbuser:dbuser@127.0.0.1:27017/devconnector",
+  secretOrKey: "The Quick brown fox jumped over the lazy hound"
 };
