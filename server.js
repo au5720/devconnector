33ef420e37b75443f0f0bdd5b977414756b16960
jennifermorgan2016 <jennifer.morgan@ise.ie>
2018-10-11 13:12:52 +0100

    Basic Mongoose MongoDb working

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
 
 const app = express();
 
-app.get("/", (req, res) => res.send("hello tere YOU"));
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("Database connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("hello World!"));
 
 // Listening PORT
 const PORT = process.env.PORT || 5000;
